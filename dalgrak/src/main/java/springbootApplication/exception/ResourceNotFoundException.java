35220b37c48578f9eb9325f2504f897fcc4f1c61
psalm156 <psalm156@naver.com>
2025-03-18 15:27:54 +0900

    test1

--- dalgrak/src/main/java/springbootApplication/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package springbootApplication.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- dalgrak/src/main/java/springbootApplication/repository/IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package springbootApplication.repository;
-
-import springbootApplication.domain.Ingredient; // 올바른 경로로 수정
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
-}
