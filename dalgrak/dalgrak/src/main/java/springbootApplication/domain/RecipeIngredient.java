0f0b68689538cf469f8f53ec49b8fa8ebc0d30af
psalm156 <psalm156@naver.com>
2025-03-18 15:44:12 +0900

    Add dalgrak

---
+++ dalgrak/dalgrak/src/main/java/springbootApplication/domain/RecipeIngredient.java
@@ -0,0 +1,56 @@
+package springbootApplication.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "recipe_ingredients")
+@IdClass(RecipeIngredient.RecipeIngredientId.class)  // @IdClass로 복합 키를 정의
+public class RecipeIngredient {
+
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private Long Id;
+
+    @ManyToOne
+    @JoinColumn(name = "recipe_id", updatable = false)
+    private Recipe recipe;
+
+    @JoinColumn(name = "ingredient_id", insertable = false, updatable = false)
+    private String ingredient;
+
+    @Column(nullable = false, length = 50)
+    private String quantity;
+
+
+    // RecipeIngredientId 클래스를 내부 클래스로 정의
+    public static class RecipeIngredientId implements Serializable {
+        
+        private Long recipeId;
+        private Long ingredientId;
+
+
+        // equals()와 hashCode() 구현
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            RecipeIngredientId that = (RecipeIngredientId) o;
+            return Objects.equals(recipeId, that.recipeId) && Objects.equals(ingredientId, that.ingredientId);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(recipeId, ingredientId);
+        }
+
+    }
+}
+
+
---
+++ dalgrak/dalgrak/src/main/java/springbootApplication/model/ChatMessage.java
@@ -0,0 +1,20 @@
+package springbootApplication.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sender;
+    private String message;
+}
