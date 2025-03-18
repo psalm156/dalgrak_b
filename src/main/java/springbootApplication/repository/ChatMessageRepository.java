da50e1d75519f064741a90e1758fc1ab78d495ce
psalm156 <psalm156@naver.com>
2025-03-18 11:52:15 +0900

    Delete src directory

--- src/main/java/springbootApplication/repository/ChatMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package springbootApplication.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import springbootApplication.model.ChatMessage;
-
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-}
--- src/main/java/springbootApplication/repository/TimerRepository.java
+++
@@ -1,8 +0,0 @@
-package springbootApplication.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import springbootApplication.domain.Timer;
-
-public interface TimerRepository extends JpaRepository<Timer, Integer> {
-}
