432e4125ad6497b3d38ca0dd52d1b321e7271bf7
psalm156 <psalm156@naver.com>
2025-03-18 14:52:07 +0900

    Ignore target directory

--- dalgrak/dalgrakproject/dalgrak/target/m2e-wtp/web-resources/META-INF/maven/dalgrakproject/dalgrak/src/main/java/springbootApplication/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package springbootApplication.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import springbootApplication.domain.Cover;
-
-public interface ImageRepository extends JpaRepository<Cover, Integer> {
-}
